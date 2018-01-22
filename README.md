4689a5f784b0efe1891f1cd42c3e6750b98bcbda
Matthew Sheppard <msheppard@funnelback.com>
2018-01-22 15:54:20 +1100

    Moved it all out of a test to make it easier to run via CLI

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 Demonstration project for reporting a case where Jackson seems to cause a JVM metaspace leak
 
-Try running it with -XX:MaxMetaspaceSize=20m - On my machine it runs happily for about a minute, then GC goes a bit crazy, loads of heap is allocated (but not used) and eventually we start getting bunches of out of memory errors.
+Try running it with...
+
+```
+> mvn clean dependency:copy-dependencies package
+> java -XX:MaxMetaspaceSize=20m -cp "target/dependency/*:target/jackson-classolader-leaker-0.0.1-SNAPSHOT.jar" com.kstruct.LeakMetaspaceViaJackson
+```
+
+ -XX:MaxMetaspaceSize=20m - On my machine it runs happily for about a minute, then GC goes a bit crazy, loads of heap is allocated (but not used) and eventually we start getting bunches of out of memory errors.
 
 I'll update this with a link to the Jackson issue once I've raised it.
---
+++ src/main/java/com/kstruct/LeakMetaspaceViaJackson.java
@@ -0,0 +1,71 @@
+package com.kstruct;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ser.DefaultSerializerProvider;
+
+import groovy.util.GroovyScriptEngine;
+
+public class LeakMetaspaceViaJackson {
+
+    private static final String GROOVY_SCRIPT_NAME_PREFIX = "Script";
+    private static final int NUM_GROOVY_SCRIPTS = 1000;
+
+    public static void main(String[] args) throws Exception {
+        leak();
+    }
+    
+    public static void leak() throws Exception {
+        File groovyClassDir = new File("target/generated_groovy_classes");
+        groovyClassDir.mkdirs();
+        
+        regenerateGroovyScripts(groovyClassDir);
+        
+        GroovyScriptEngine engine = new GroovyScriptEngine(groovyClassDir.getAbsolutePath());
+        ObjectMapper mapper = new ObjectMapper();
+        
+        while(true) {
+            regenerateGroovyScripts(groovyClassDir);
+
+            for (int i = 0; i < NUM_GROOVY_SCRIPTS; i++) {
+                String groovyScriptName = GROOVY_SCRIPT_NAME_PREFIX + i + ".groovy";
+
+                Class groovyClass = engine.loadScriptByName(groovyScriptName);
+
+                Object groovyObject = groovyClass.newInstance();
+                
+                String json = mapper.writeValueAsString(groovyObject);
+                
+                System.out.println(json);
+            }
+
+            // Note that we haven't kept any reference to any groovyClasses or
+            // groovyObjects, so they should be collectable in both the heap
+            // and meta-space.
+
+            // https://github.com/FasterXML/jackson-databind/issues/489
+            // suggests we may be expected to call this if we're dynamically
+            // creating types
+            mapper.getTypeFactory().clearCache();
+            
+            // If found that clearing this helps a bit (but doesn't totally fix the problem)
+            System.out.println(((DefaultSerializerProvider) mapper.getSerializerProvider()).cachedSerializersCount());
+            ((DefaultSerializerProvider) mapper.getSerializerProvider()).flushCachedSerializers();
+        }
+    }
+
+    /**
+     * Generate 1000 groovy script files which GroovyScriptEngine will reload (because the
+     * modification time changed).
+     */
+    private static void regenerateGroovyScripts(File groovyClassDir) throws IOException {
+        for (int i = 0; i < NUM_GROOVY_SCRIPTS; i++) {
+            File groovyScriptFile = new File(groovyClassDir, GROOVY_SCRIPT_NAME_PREFIX + i + ".groovy");
+            byte[] groovyScriptContent = ("public class Foo { public String name = \"NUMBER-" + i + "\"; }").getBytes();
+            Files.write(groovyScriptFile.toPath(), groovyScriptContent);
+        }
+    }
+}
